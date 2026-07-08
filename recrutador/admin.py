b733c313c23bf23e8405ec7402d9e1d3632ebc51
Islan <islan12victhor@gmail.com>
2026-07-07 21:46:59 -0300

    feat: adiciona os models a pagina admin

--- recrutador/admin.py
+++ recrutador/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Empresa, Recrutador, ProcessoSeletivo, ProcessoTemFuncao, CandidatoInscritoProcesso
 # Register your models here.
+
+admin.site.register(Empresa)
+admin.site.register(Recrutador)
+admin.site.register(ProcessoSeletivo)
+admin.site.register(ProcessoTemFuncao)
+admin.site.register(CandidatoInscritoProcesso)
--- vagas/admin.py
+++ vagas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Candidato, Funcao, CandidatoTemFuncao, TelefoneCandidato
 # Register your models here.
+
+admin.site.register(Candidato)
+admin.site.register(Funcao)
+admin.site.register(CandidatoTemFuncao)
+admin.site.register(TelefoneCandidato)
