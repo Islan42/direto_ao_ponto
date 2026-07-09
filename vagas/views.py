383672ee3ac971a559390ab4b39267212572164f
Islan <islan12victhor@gmail.com>
2026-07-09 17:25:33 -0300

    refactor: aprimora as primeiras views do app vagas

--- vagas/views.py
+++ vagas/views.py
@@ -4,21 +4,34 @@ from .models import Candidato
 from recrutador.models import ProcessoSeletivo, Empresa
 
 def index(request):
-    vagas = get_list_or_404(ProcessoSeletivo, ativo = True)
-    lista_vagas = [vaga.__str__() for vaga in vagas]
-    return HttpResponse(f"{lista_vagas}")
+    vagas = get_list_or_404(ProcessoSeletivo, ativo = True) #ORDER BY
+    context = {
+        "vagas" : vagas,
+        }
+
+    return render(request, "vagas/index.html", context)
 
 def perfil_processo(request, processo_id):
     processo = get_object_or_404(ProcessoSeletivo, pk = processo_id)
-    return HttpResponse(f"{processo.titulo}\nRemuneração: R${processo.remuneracao}\n{processo.cidade}")
+    context = {
+        "processo" : processo,
+    }
+    return render(request, "vagas/perfil_processo.html", context)
 
 def new_candidato(request):
     return HttpResponse("CADASTRANDO NOVO USUÁRIO")
 
 def perfil_candidato(request, candidato_id):
     candidato = get_object_or_404(Candidato, pk = candidato_id)
-    return HttpResponse(f"{candidato.nome} - {candidato.email} - {candidato.cidade} - {candidato.resumo_profissional}")
+    context = {
+        "candidato" : candidato,
+    }
+    return render(request, "vagas/perfil_candidato.html", context)
+    
 
 def perfil_empresa(request, empresa_id):
     empresa = get_object_or_404(Empresa, pk = empresa_id)
-    return HttpResponse(f"{empresa.razao_social} ({empresa.cnpj}) - {empresa.cidade}/{empresa.estado}")
+    context = {
+        "empresa" : empresa
+    }
+    return render(request, "vagas/perfil_empresa.html", context)
